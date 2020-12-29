e1acfe7055a407219acb25be8cb028eb01b777ad
asharali95 <52744991+asharali95@users.noreply.github.com>
2020-12-29 22:44:01 +0500

    commit on 'new_branch'

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log(global)
+
+// setTimeout(() => {
+//   console.log("in timeout");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log("in set Interval");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// -----------------------
+// Modules and require
+
+// const { ages, people } = require("./people");
+// ages.forEach((age) => {
+//   console.log(age);
+// });
+// people.forEach((p) => {
+//   console.log(p);
+// });
+
+// OS
+const os = require("os");
+
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ["syed", "ashar", "ali"];
+const ages = [20, 21, 22];
+// console.log(module)
+module.exports = {
+  people,
+  ages,
+};
